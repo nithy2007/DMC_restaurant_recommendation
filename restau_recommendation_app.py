e8c0d4bb4eae2e16091785efec8f3bda6817ff08
nithy2007 <129135219+nithy2007@users.noreply.github.com>
2025-07-31 21:18:36 +0530

    Update restau_recommendation_app.py
    debug=True

--- restau_recommendation_app.py
+++ restau_recommendation_app.py
@@ -63,4 +63,4 @@ def recommend():
                                                             selected_cuisine=cuisine)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000,debug=True)
